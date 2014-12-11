c386f743e671d292eec67106de2a2080527715ad
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2014-12-11 19:22:50 +0100

    instagram

--- app/scripts/controllers/instagram.js
+++ app/scripts/controllers/instagram.js
@@ -9,20 +9,20 @@
  */
 angular.module('mihApp')
   .controller('InstagramCtrl', function ($scope,Fileloader) {
-        Fileloader.getFile("data/instagram/result2.json").then(function(data){
+        Fileloader.getFile("data/instagram/result3.json").then(function(data){
             //console.log(data);
             $scope.see = true;
 
 
-           /* var lol = d3.nest()
+            var lol = d3.nest()
                 .key(function(d){return d.nil})
                 .entries(data);
 
             console.log(lol);
-*/
+
 
             data.forEach(function(d){
-                var col = d3.rgb(parseInt(d.r), parseInt(d.g), parseInt(d.b));
+                var col = d3.rgb(d.color);
                 var hsl = col.hsl();
                 d.h = hsl.h;
                 d.s = hsl.s;
